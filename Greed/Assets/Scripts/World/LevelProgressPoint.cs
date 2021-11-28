0fc6f719dcdf9eedd9e0522c42ac921f05c6677e
pascal <pmkgbwis@gmail.com>
2021-11-27 18:43:54 -0600

    more game feel

--- Greed/Assets/Scripts/World/LevelProgressPoint.cs
+++ Greed/Assets/Scripts/World/LevelProgressPoint.cs
@@ -6,9 +6,11 @@ public class LevelProgressPoint : MonoBehaviour
 {
     private int hp = 3;
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject dustPrefab;
 
     private void OnTriggerEnter2D(Collider2D other) {
         hp--;
+        Instantiate(dustPrefab, transform);
         animator.SetTrigger("Interact");
         if (hp < 0) {
             FindObjectsOfType<LevelDirector>()[0].NextLevel();
