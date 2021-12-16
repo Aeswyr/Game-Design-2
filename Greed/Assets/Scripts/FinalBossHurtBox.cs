817d22981f50b2cd8ed72c5cd0d3aae8856f4f72
pascal <pmkgbwis@gmail.com>
2021-12-15 20:16:09 -0600

    Boss rev 1

--- Greed/Assets/Scripts/FinalBossHurtBox.cs
+++ Greed/Assets/Scripts/FinalBossHurtBox.cs
@@ -6,6 +6,9 @@ public class FinalBossHurtBox : MonoBehaviour
 {
     [SerializeField] int health; 
     [SerializeField] GameObject gPickupPrefab;
+    [SerializeField] private GameObject hitspark;
+    [SerializeField] private GameObject dustPrefab;
+    [SerializeField] private FinalBossMovement boss;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.TryGetComponent(out Projectile proj)) {
             if (!proj.IsValidTarget(gameObject))
@@ -28,7 +31,17 @@ public class FinalBossHurtBox : MonoBehaviour
             GemBurst(99);
             FindObjectsOfType<LevelDirector>()[0].StartGameEndSequence();
         }
-        Debug.Log("WOO I HIT THE BOSS"); 
+        
+        Transform t1 = transform.parent;
+        Transform t2 = other.transform;
+        if (other.transform.parent != null)
+            t2 = other.transform.parent;
+        Instantiate(hitspark, 0.5f * (transform.position + other.transform.position), hitspark.transform.rotation);
+        Instantiate(dustPrefab, t1);
+        boss.Hurt();
+        EffectsMaster.Instance.ScreenShake(0.5f, 0.2f);
+        if (t2.TryGetComponent(out PlayerController p2))
+            p2.Pause(0.2f);
     }
     public void GemBurst(int drop) {
         for (int i = 0; i < drop / 10; i++){
@@ -47,4 +60,5 @@ public class FinalBossHurtBox : MonoBehaviour
         newGem.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-15f, 15f), Random.Range(20f, 40f)), ForceMode2D.Impulse);
         newGem.GetComponent<ItemPickup>().SetType(type);
     }
+    
 } 
--- Greed/Assets/Scripts/FinalBossMovement.cs
+++ Greed/Assets/Scripts/FinalBossMovement.cs
@@ -6,45 +6,116 @@ public class FinalBossMovement : MonoBehaviour
 {
     [SerializeField] private int speedX;
     [SerializeField] private GameObject punchPrefab;
-    [SerializeField] private float PunchDelay;
+    [SerializeField] private float attackDelay;
     [SerializeField] private LayerMask mask;
     [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject slamPrefab;
     [SerializeField] private Collider2D hurtBox;
-    private float punchTime;
-    private int speedY;
+    [SerializeField] private Animator animator;
+    private float attackTime;
     private int directionX;
-    private int directionY;
-    
-   
-    private Vector2 movement;
-    private bool collided = false;
     [SerializeField] private Rigidbody2D rbody;
  
      void Start () {
-         //speedY = Random.Range (1,);
          directionX = 1;
-         //directionY = Random.Range (-1, 1)
         
          
      }
  
      void FixedUpdate() {
+
+        CheckPaused();
+
+        if(Time.time >= attackTime + attackDelay){ 
+            attackTime = Time.time;
+            int val = Random.Range(0, 3);
+            switch(val) {
+                case 0:
+                    animator.SetTrigger("slam");
+                    break;
+                case 1:
+                    animator.SetTrigger("ranged");
+                    break;
+                case 2:
+                    animator.SetTrigger("melee");
+                    break;
+            }
+        }
+     }
+
+    public void Hurt() {
+        Pause(0.2f);
+    }
+
+    public void Melee() {
+        GameObject G = Instantiate(punchPrefab, transform); 
+        G.transform.localPosition = new Vector3(8, -3, 0);
+    }
+
+    public void Ranged() {
+        GameObject G = Instantiate(projectilePrefab, transform.position + new Vector3(0, -6.3f, 0), projectilePrefab.transform.rotation);
+        G.GetComponent<Projectile>().Init(new Vector2(directionX, 0), hurtBox); 
+    }
+
+    public void Slam() {
+        GameObject G = Instantiate(punchPrefab, transform); 
+        G.transform.localPosition = new Vector3(0, -3, 0);
+
+        int spikes = Random.Range(3, 6);
+        int xoffset = 0;
+        int dist = 16;
+        int yoffset = 0;
+        switch (spikes) {
+            case 3:
+                xoffset = -16;
+                yoffset = 15;
+                break;
+            case 4:
+                xoffset = -24;
+                yoffset = 23;
+                break;
+            case 5:
+                xoffset = -32;
+                yoffset = 23;
+                break;
+        }
+        for (int i = 0; i < spikes; i++) {
+            GameObject g = Instantiate(slamPrefab, new Vector3(xoffset + i * dist, yoffset, 0), slamPrefab.transform.rotation);
+            g.GetComponent<Projectile>().Init(new Vector2(1, 0), hurtBox); 
+        }
+    }
+
+    public void Move() {
         rbody.velocity = new Vector2 (speedX * directionX, rbody.velocity.y); 
         RaycastHit2D l = Utils.Raycast(transform.position, Vector2.left, 12, mask); 
         RaycastHit2D r = Utils.Raycast(transform.position, Vector2.right, 12, mask);
         if((l && directionX < 0) || (r && directionX > 0)){
-            Debug.Log("AHHHHHHHHHH");
-            directionX *= -1;   
-
+            directionX *= -1;
+            Vector3 scale = transform.localScale;
+            scale.x = directionX;
+            transform.localScale = scale;
         } 
-        if(Time.time >= punchTime + PunchDelay){ 
-            punchTime = Time.time;
-            GameObject G = Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-            G.GetComponent<Projectile>().Init(new Vector2(directionX, 0), hurtBox); 
-            /*GameObject G = Instantiate(punchPrefab, transform); 
-            G.transform.localPosition = new Vector3(10, 0, 0);*/
+    }
+
+    bool paused = false;
+    float pauseTime;
+    Vector2 pauseVelocity;
+    public void Pause(float duration) {
+        pauseTime = Time.time + duration;
+        paused = true;
+        pauseVelocity = rbody.velocity;
+        rbody.velocity = Vector2.zero;
+        animator.speed = 0;
+    }
+
+    private void CheckPaused() {
+        if (paused && Time.time >= pauseTime) {
+            paused = false;
+            rbody.velocity = pauseVelocity;
+            animator.speed = 1;
         }
-     } 
+    }
+
  
  }  
 
